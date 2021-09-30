721fb89ec3d3bfe7c0b6540452c4c095088e0f35
Leo DEVIN <leo.devin@epita.fr>
2021-09-30 18:07:26 +0200

    fonction xor mais jsp si cest ca

---
+++ src/neural_network/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "xor.h"
+
+int main()
+{
+	printf("xor(%i, %i) = %i\n", 0, 0, xor(0, 0));
+	return 0;
+}
---
+++ src/neural_network/xor.c
@@ -0,0 +1,11 @@
+int xor(int x1, int x2)
+{
+	if (x1 != x2)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
