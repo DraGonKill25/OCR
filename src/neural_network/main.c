f33072f986145e7f52647b6910638e68958aa64e
Leo DEVIN <leo.devin@epita.fr>
2021-10-01 10:53:12 +0200

    deleted

--- src/neural_network/main.c
+++ src/neural_network/main.c
@@ -1,8 +1,1 @@
-#include <stdio.h>
-#include "xor.h"
 
-int main()
-{
-	printf("xor(%i, %i) = %i\n", 0, 0, xor(0, 0));
-	return 0;
-}
--- src/neural_network/xor.c
+++ src/neural_network/xor.c
@@ -1,11 +1,1 @@
-int xor(int x1, int x2)
-{
-	if (x1 != x2)
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
+
