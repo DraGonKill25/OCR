3172a46c3dea48e18e5cfc9d35db974c3a27ec54
Hugo RATTE <hugo.ratte@epita.fr>
2021-10-12 14:56:31 +0200

    DEBUT reconnaisance image

--- src/Image_SPLITTING/Image_Splitting.c
+++ src/Image_SPLITTING/Image_Splitting.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
 #include <SDL.h>
 
+SDL_Surface * colorTreatment(SDL_Surface *image);
+Uint32 blackAndwhite(Uint32 Pixel, SDL_PixelFormat *Format);
 
-int x0 = x0;
+int *_xgrille = NULL;
+int *_ygrille = NULL;
+int *_final = NULL;
+int *_yfinal = NULL;
+
+//Functions
+SDL_Surface * colorTreatment(SDL_Surface *image)
+{
+	int i, j;
+	SDL_LockSurface(image);
+	Uint32 *pixels = image->pixels;
+	int h = image->h;
+	int w = image->w;
+	SDL_PixelFormat *Format = image->format;
+	for(i = 0; i < h; i++)
+	{
+    	for(j = 0; j < w; j++)
+		{
+        	pixels[i * w + j] = blackAndwhite(pixels[i * w + j], Format);
+		}
+	}
+	SDL_UnlockSurface(image);
+	return image;
+}
+
+
+
+
+
 int distance_L(int x0,int x1)
 {
     return x1-x0;
@@ -11,7 +41,7 @@ int distance_L(int x0,int x1)
 // Return the list of all Images Splitting
 // This list is a matrix[9][9], the images are organised like in The Array initial 
 // x0 = the most left x, x1 = the most right x, y0 = the most top y, y1 = the most low y 
-array[][] list_Splitting(SDL_Rect picture)
+array[][] list_Splitting(SDL_Surface *image)
 {
     array[9][9] new_List;
     int L = distance_L(int x0,int x1);
@@ -51,4 +81,54 @@ SDL_BlitSurface IMAGE_SPLITTING(SDL_Rect srcrect,int x,int y)
     dstrect.h = distance_L(int x0,int x1)/9;
 
     return SDL_BlitSurface(src, &srcrect, dst, &dstrect); 
+}
+
+int search_grille(SDL_Surface *image)
+{
+    int xgrille = &_xgrille;
+    int ygrille = &_ygrille;
+
+    while (ygrille < height)
+    {
+        xgrille = _xgrille;
+        while (xgrille < width):
+        {
+            if Good_research(*image,xgrille,ygrille)
+                return (xgrille,ygrille)
+            xgrille++;
+
+        }
+        ygrille++;
+    }
+}
+
+int Good_research(SDL_Surface *image,int x,int y)
+{
+    Uint32 *pixels = image->pixels;
+    int w = image->w;
+    SDL_PixelFormat *Format = image->format;
+    if (BlackOrWhite(pixels[y * w + x], Format))
+        {
+            return 0;
+        }
+    else
+        {
+        }
+
+}
+
+int BlackOrWhite(Uint32 Pixel, SDL_PixelFormat *Format)
+{
+	Uint8 r;
+	Uint8 g;
+	Uint8 b;
+	SDL_GetRGB(Pixel, Format, &r, &g, &b);
+	if ((r + g + b) / 3 == 255)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
 }
