d44e7cd5fdfd555f247708a378d06e90e6082dd8
Rabouille Mathis <mathis.rabouille@epita.fr>
2021-10-12 13:48:08 +0200

    Rotation et couleur fini,mais bug de malloc

--- src/image_treatment/image_treatment.c
+++ src/image_treatment/image_treatment.c
@@ -2,10 +2,15 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
+#include <err.h>
+#include <math.h>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+//Constants
+const double M_PI = 3.14159265358979323846;
 
 //Functions
 SDL_Surface * colorTreatment(SDL_Surface *image);
@@ -14,7 +19,7 @@ Uint32 blackAndwhite(Uint32 Pixel, SDL_PixelFormat *Format);
 
 SDL_Surface * colorTreatment(SDL_Surface *image)
 {
-	size_t i, j;
+	int i, j;
 	SDL_LockSurface(image);
 	Uint32 *pixels = image->pixels;
 	int h = image->h;
@@ -22,12 +27,77 @@ SDL_Surface * colorTreatment(SDL_Surface *image)
 	SDL_PixelFormat *Format = image->format;
 	for(i = 0; i < h; i++)
 	{
-    		for(j = 0; j < w; j++)
+    	for(j = 0; j < w; j++)
 		{
-        		pixels[i * w + j] = blackAndwhite(pixels[i * w + j], Format);
+        	pixels[i * w + j] = blackAndwhite(pixels[i * w + j], Format);
 		}
 	}
+	SDL_UnlockSurface(image);
 	return image;
+}
+
+/* SDL_Surface * surfaceRotation(SDL_Surface *image, double angle)
+{
+    int i, j;
+   // SDL_PixelFormat* Imgformat = image->format;
+    SDL_Surface *Returned = 
+    SDL_CreateRGBSurfaceFrom(image->pixels,image->w,image->h,32,image->pitch,0,0,0,0);
+    SDL_LockSurface(Returned);
+    Uint32 *Dstpixels = Returned->pixels;
+    Uint32 *Srcpixels = image->pixels;
+    int w = image->w;
+    int h = image->h;
+
+    for(i = 0; i < h; i++)
+    {
+        for(j = 0; j < w; j++)
+        {
+
+            Dstpixels[i * w + j] = Srcpixels[(i*(int)cos(angle)-j*(int)sin(angle)) * w +
+            (j*(int)cos(angle) + i*(int)sin(angle))];
+        }
+    }
+    SDL_UnlockSurface(Returned);
+    return Returned;
+}
+*/
+
+void Rotate(SDL_Surface* img, double angle)
+{
+    //int n_w, n_h;
+
+    //GetNewSize(img, angle, &n_h, &n_w);
+
+    SDL_Surface* new_img = SDL_CreateRGBSurface(0, img -> w, img -> h, 32, 0, 0, 0, 0);
+    int center_x = img -> w / 2;
+    int center_y = img -> h / 2;
+    Uint32 *Dstpixels = new_img->pixels;                                       
+    Uint32 *Srcpixels = img->pixels;   
+    SDL_LockSurface(new_img);
+    angle = angle * M_PI / 180;
+
+    for (int i = 0; i < img -> w; i++)
+    {
+        for (int j = 0; j < img -> h; j++)
+        {
+            int x = (int) ((i - center_x) * cos(angle) - (j - center_y) * sin(angle) + center_x);
+            int y = (int) ((i - center_x) * sin(angle) + (j - center_y) * cos(angle) + center_y);
+
+            if (x >= 0 && y >= 0 && x < img -> w && y < img -> h)
+            {
+                Uint32 pixel = Srcpixels[j * (img->w) + i]; //get_pixel(img, x, y);
+                Dstpixels[y * (img->w) + x] = pixel;        //put_pixel(new_img, i, j, pixel);
+            }
+            else
+            {
+                Uint32 pixel = SDL_MapRGB(img -> format, 255, 255, 255);
+                Dstpixels[j * (img->w) + i] = pixel;        //put_pixel(new_img, i, j, pixel);
+            }
+        }
+    }   
+    SDL_UnlockSurface(new_img);
+    *img = *new_img;
+    SDL_FreeSurface(new_img);
 }
 
 Uint32 blackAndwhite(Uint32 Pixel, SDL_PixelFormat *Format)
@@ -36,19 +106,29 @@ Uint32 blackAndwhite(Uint32 Pixel, SDL_PixelFormat *Format)
 	Uint8 g;
 	Uint8 b;
 	SDL_GetRGB(Pixel, Format, &r, &g, &b);
-	if ((r + g + b) / 3 > 127)
-        {
-                return SDL_MapRGB(Format, 255, 255, 255);
-        }
-        else
-        {
-        	return SDL_MapRGB(Format, 0, 0, 0);
-        }
+	if ((r + g + b) / 3 > 200)
+    {
+        return SDL_MapRGB(Format, 255, 255, 255);
+    }
+    else
+    {
+        return SDL_MapRGB(Format, 0, 0, 0);
+    }
 }
 
 
 int main( int argc, char* args[] )
 {
+    if (argc)
+    {
+        
+    }
+    if (args)
+    {
+        
+    }
+
+
     //The window we'll be rendering to
     SDL_Window * window = NULL;
 
@@ -58,9 +138,6 @@ int main( int argc, char* args[] )
     //The surface loaded
     SDL_Surface * Loaded = NULL;
 
-    //Treated surface
-    SDL_Surface * NewLoaded = NULL;
-
     //Initialize SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
@@ -69,47 +146,52 @@ int main( int argc, char* args[] )
     else
     {
         //Load image
-	Loaded=IMG_Load("image_03.jpeg");
-	if(!Loaded) 
-	{
+	    Loaded=IMG_Load("image_03.jpeg");
+	    if(!Loaded) 
+	    {
     		printf("IMG_Load: %s\n", IMG_GetError());
-	}	
-	else
-	{
-		//Create window
+	    }	
+	    else
+	    {
+		    //Create window
         	window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,Loaded->w , Loaded->h, SDL_WINDOW_SHOWN );
         	if( window == NULL )
         	{
-            		printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            	printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
         	}
-		else
+		    else
         	{
-            		//Get window surface
-            		screenSurface = SDL_GetWindowSurface( window );
+            	//Get window surface
+            	screenSurface = SDL_GetWindowSurface( window );
 
 	    
 	    		//Blit the Loaded image over the window's surface
-			SDL_BlitSurface(Loaded, NULL, screenSurface, NULL);
+			    SDL_BlitSurface(Loaded, NULL, screenSurface, NULL);
 
-            		//Update the surface
-            		SDL_UpdateWindowSurface( window );
-			SDL_Delay(5000);
+            	//Update the surface
+            	SDL_UpdateWindowSurface( window );
+			    SDL_Delay(2000);
 
-			//Treat the loaded image
-			NewLoaded = colorTreatment(Loaded);
+			    //Treat the loaded image
+			    colorTreatment(Loaded);
 
-			SDL_BlitSurface(NewLoaded, NULL, screenSurface, NULL);
-			SDL_UpdateWindowSurface( window );
+			    SDL_BlitSurface(Loaded, NULL, screenSurface, NULL);
+			    SDL_UpdateWindowSurface( window );
+
+                SDL_Delay(2000);
+
+                Rotate(Loaded, 90);
+
+                SDL_BlitSurface(Loaded, NULL, screenSurface, NULL);
+                SDL_UpdateWindowSurface( window );
 
 
-            		//Wait two seconds
-            		SDL_Delay( 5000 );
-		}
-	}
+            	//Wait two seconds
+            	SDL_Delay( 2000 );
+		    }
+	    }
     }
     SDL_FreeSurface( Loaded );
-    SDL_FreeSurface( NewLoaded );
-    NewLoaded = NULL;
     Loaded = NULL;
 
     //Destroy window
